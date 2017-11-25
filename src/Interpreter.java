e9a2a2bae8789c554395325c8a71791c67d9fa25
Szymon Kocur <skocur10@gmail.com>
2017-11-25 12:51:30 +0100

    Added comments
    To make our .rx apps looks pretty.

--- src/Interpreter.java
+++ src/Interpreter.java
@@ -10,24 +10,24 @@ public class Interpreter implements IInterpreter {
 	@Override
 	public void scan(List<String> expressions) throws UnknownExpressionException {
 
-		//TODO: Add more types of variables
-
 		for(String expression : expressions){
-			String function = expression.substring(0, expression.indexOf(' '));
-			String functionParams = expression.substring(expression.indexOf(' ') + 1);
+			if(!expression.startsWith("//") && !expression.isEmpty()){
+				String function = expression.substring(0, expression.indexOf(' '));
+				String functionParams = expression.substring(expression.indexOf(' ') + 1);
 
-			switch(function){
-				case "log":
-					if(XString.xStrings.get(functionParams) != null)
-						Log.execute(XString.xStrings.get(functionParams));
-					else
-						Log.execute(functionParams);
-					break;
-				case "init":
-					Init.initializeVariable(expression);
-					break;
-				default:
-					throw new UnknownExpressionException();
+				switch(function){
+					case "log":
+						if(XString.xStrings.get(functionParams) != null)
+							Log.execute(XString.xStrings.get(functionParams));
+						else
+							Log.execute(functionParams);
+						break;
+					case "init":
+						Init.initializeVariable(expression);
+						break;
+					default:
+						throw new UnknownExpressionException();
+				}
 			}
 		}
 	}
