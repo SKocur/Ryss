1bdd9de0a4eb15630d753f88916d7c5048a844bb
Szymon Kocur <skocur10@gmail.com>
2017-11-26 14:59:36 +0100

    Add passing arguments through command line

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		try {
-			File file = new File("examples/hello_world.rx");
+			File file = new File(args[0]);
 			Scanner scanner = new Scanner(file);
 
 			while(scanner.hasNext()) {
